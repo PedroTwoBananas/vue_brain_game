7cf0e3141f9990d3bb3b9e6529cea91fc8bc83e5
PedroTwoBananas <paradaigor20@gmail.com>
2022-03-12 19:48:19 +0300

    верстка меню настроек

--- src/components/pages/Main.vue
+++ src/components/pages/Main.vue
@@ -1,5 +1,47 @@
 <template>
-   <div>привет</div>
+   <div>
+      <div><h1>Привет!</h1></div>
+      <div>
+         <p>
+            Добро пожаловать на 24 тренировочный день. Ваш последний результат -
+            решено "заглушка" из "заглушка". Общая точность - 80%.
+         </p>
+      </div>
+      <div>
+         <h2>Настройки</h2>
+         <div>
+            <input type="range" />
+            <span>Длительность "заглушка" минут</span>
+         </div>
+         <div>
+            <input type="range" />
+            <span>Сложность "заглушка"</span>
+         </div>
+      </div>
+      <div>
+         <div>
+            <input type="checkbox" />
+            <span>Суммирование</span>
+         </div>
+         <div>
+            <input type="checkbox" />
+            <span>Разность</span>
+         </div>
+         <div>
+            <input type="checkbox" />
+            <span>Умножение</span>
+         </div>
+         <div>
+            <input type="checkbox" />
+            <span>Деление</span>
+         </div>
+         <div>
+            <input type="checkbox" />
+            <span>Возведение в степень</span>
+         </div>
+      </div>
+      <button @click="this.$router.push({ name: 'game' })">Play!</button>
+   </div>
 </template>
 
 <script>
