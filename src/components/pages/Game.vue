b6e3fdfc5d6612b4b68e0fe26bb6a7d46c3bf272
PedroTwoBananas <paradaigor20@gmail.com>
2022-03-14 15:16:28 +0300

    реализация прочтения пользовательских чисел в выражении, решения и его проверки

--- src/components/pages/Game.vue
+++ src/components/pages/Game.vue
@@ -1,25 +1,30 @@
 <template>
-   <div class="expression">
-      <div v-for="sign in generator()">
-         <span
-            v-if="
-               (!sign.hidden && sign.type === 'number') ||
-               sign.type === 'operator'
-            "
-            >{{ sign.value }}</span
-         >
-         <input class='sign-input' v-if="sign.hidden" />
-         <span v-if="sign.type === 'total'">= {{ sign.value }}</span>
+   <div>
+      <div class="expression">
+         <div v-for="sign in data.inputExpression">
+            <span v-if="!sign.hidden && sign.type !== 'total'">{{
+               sign.value
+            }}</span>
+            <input
+               type="number"
+               class="sign-input"
+               v-model="sign.value"
+               v-if="sign.hidden"
+            />
+            <span v-if="sign.type === 'total'">= {{ sign.value }}</span>
+         </div>
       </div>
+      <button @click="showExpression">Показать выражение</button>
+      <button @click="checkSolution">Проверить</button>
    </div>
 </template>
 
 <script>
 import { useStore } from 'vuex'
+import { ref } from 'vue'
 
 export default {
    setup() {
-
       const store = useStore()
 
       const configs = store.state.configs
@@ -30,7 +35,7 @@ export default {
          return Math.floor(rand)
       }
 
-      const generator = () => {
+      const generate = () => {
          const difficulty = +configs.difficulty
 
          const operators = configs.selectedOperators
@@ -73,7 +78,61 @@ export default {
 
          return expression
       }
-      return { generator }
+
+      const data = ref({
+         generatedExpression: generate(),
+         inputExpression: [],
+      })
+
+      data.value.inputExpression = data.value.generatedExpression.map(
+         (sign) => {
+            if (sign.hidden && sign.type === 'number') {
+               const newSign = { ...sign }
+               newSign.value = null
+               return newSign
+            }
+            return sign
+         }
+      )
+
+      const showExpression = () => {
+         const exp = data.value.generatedExpression
+            .filter((elem) => elem.type !== 'total')
+            .map((elem) => elem.value.toString())
+            .join('')
+
+         return alert(exp)
+      }
+
+      const checkSolution = () => {
+         const solution = data.value.generatedExpression.find(
+            (sign) => sign.type === 'total'
+         ).value
+
+         const leftIdentity = data.value.inputExpression
+            .filter((elem) => elem.type !== 'total')
+            .map((elem) => elem.value.toString())
+            .join('')
+
+         const rightIdentity = eval(leftIdentity)
+
+         rightIdentity === solution ? alert('Молодец!') : alert('Болван!')
+
+         data.value.generatedExpression = generate()
+
+         data.value.inputExpression = data.value.generatedExpression.map(
+            (sign) => {
+               if (sign.hidden && sign.type === 'number') {
+                  const newSign = { ...sign }
+                  newSign.value = null
+                  return newSign
+               }
+               return sign
+            }
+         )
+      }
+
+      return { data, checkSolution, showExpression }
    },
 }
 </script>
